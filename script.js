4bc297eb1cb334e05a378a5b9b2f5e4b50c997e0
Hizotaro <111581734+Hizotaro@users.noreply.github.com>
2026-08-20 21:07:17 +0200

    feat: add computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const randomIndex = Math.floor(Math.random() * 3);
+
+    if (randomIndex === 0) {
+        return "rock";
+    } else if (randomIndex === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
